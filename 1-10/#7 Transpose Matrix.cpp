f45d70b3f057e11335db80ab3f70f0a30e463215
Ahmed Saied <87888343+Hamoudidev@users.noreply.github.com>
2025-09-14 00:42:36 +0300

    Create #7 Transpose Matrix.cpp

---
+++ 1-10/#7 Transpose Matrix.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int RandomNumber(int From, int To) {
+	int number = rand() % (To - From + 1) + From;
+
+	return number;
+}
+
+void FillOrderedMatrix(int array[3][3], short Rows, short Cols) {
+	int Counter = 1;
+	for (short i = 0; i < Rows; i++) {
+		for (short j = 0; j < Cols; j++) {
+			array[i][j] = Counter;
+			Counter++;
+		}
+	}
+}
+
+void TranposeMatrix(int array[3][3], int arrTransposed[3][3], short Rows, short Cols) {
+	for (short i = 0; i < Rows; i++) {
+		for (short j = 0; j < Cols; j++) {
+			arrTransposed[j][i] = array[i][j];
+		}
+	}
+}
+
+void PrintMatrix(int array[3][3], short Rows, short Cols) {
+	for (short i = 0; i < Rows; i++) {
+		for (short j = 0; j < Cols; j++) {
+			cout << setw(3) << array[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+
+int main() {
+	srand((unsigned)time(NULL));
+	int array[3][3];
+	int arrTransposed[3][3];
+
+	FillOrderedMatrix(array, 3, 3);
+	cout << "Following Matrix With Ordered Numbers :" << endl;
+	PrintMatrix(array, 3, 3);
+
+	TranposeMatrix(array, arrTransposed, 3, 3);
+	cout << "Following Matrix With Transposed Numbers :" << endl;
+	PrintMatrix(arrTransposed, 3, 3);
+
+	return 0;
+}
