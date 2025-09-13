58acb6190539b75bd1df3697a656dba7cfcf1426
Ahmed Saied <87888343+Hamoudidev@users.noreply.github.com>
2025-09-14 00:35:21 +0300

    Create #6 3x3 Matrix Ordered Numbers.cpp

---
+++ 1-10/#6 3x3 Matrix Ordered Numbers.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int RandomNumber(int From, int To) {
+	int number = rand() % (To - From + 1) + From;
+
+	return number;
+}
+
+void FillMatrix(int array[3][3], short Rows, short Cols) {
+	int Counter = 1;
+	for (short i = 0; i < Rows; i++) {
+		for (short j = 0; j < Cols; j++) {
+			array[i][j] = Counter;
+			Counter++;
+		}
+	}
+}
+
+void PrintMatrix(int array[3][3], short Rows, short Cols) {
+	for (short i = 0; i < Rows; i++) {
+		for (short j = 0; j < Cols; j++) {
+			cout << setw(3) << array[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+
+
+int main() {
+	srand((unsigned)time(NULL));
+	int array[3][3];
+
+	FillMatrix(array, 3, 3);
+	cout << "Following Matrix With Ordered Numbers :" << endl;
+	PrintMatrix(array, 3, 3);
+
+	return 0;
+}
