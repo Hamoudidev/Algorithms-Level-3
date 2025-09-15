fb7003dc15f20cdd078783ef41ea38ef46e139ce
Ahmed Saied <87888343+Hamoudidev@users.noreply.github.com>
2025-09-15 19:55:06 +0300

    Create Check Equal Typical Matrix.cpp

---
+++ 11-20/Check Equal Typical Matrix.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int RandomNumber(int From, int To) {
+	int number = rand() % (To - From + 1) + From;
+
+	return number;
+}
+
+void FillMatrix(int array[3][3], short Rows, short Cols) {
+	for (short i = 0; i < Rows; i++) {
+		for (short j = 0; j < Cols; j++) {
+			array[i][j] = RandomNumber(1, 10);
+		}
+	}
+}
+
+void PrintMatrix(int array[3][3], short Rows, short Cols) {
+	for (short i = 0; i < Rows; i++) {
+		for (short j = 0; j < Cols; j++) {
+			printf(" %0*d   ", 2, array[i][j]);
+		}
+		cout << endl;
+	}
+}
+
+int SumMatrix(int array[3][3], short Rows, short Cols) {
+	int Sum = 0;
+	for (short i = 0; i < Rows; i++) {
+		for (short j = 0; j < Cols; j++) {
+			Sum += array[i][j];
+		}
+	}
+
+	return Sum;
+}
+
+bool checkEqualTypical(int array[3][3], int array2[3][3], short Rows, short Cols) {
+	for (short i = 0; i < Rows; i++) {
+		for (short j = 0; j < Cols; j++) {
+			if (array[i][j] != array2[i][j]) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+void printCheck(bool isEqualTypical) {
+	if (isEqualTypical)
+		cout << "\nYes: Matrices are Equal Typical" << endl;
+	else
+		cout << "\nNo: Matrices are NOT Equal Typical" << endl;
+}
+
+int main() {
+	srand((unsigned)time(NULL));
+	int array[3][3];
+	int array2[3][3];
+
+	FillMatrix(array, 3, 3);
+	cout << "Matrix 1 :" << endl;
+	PrintMatrix(array, 3, 3);
+
+	FillMatrix(array2, 3, 3);
+	cout << "Matrix 2 :" << endl;
+	PrintMatrix(array2, 3, 3);
+
+	printCheck(checkEqualTypical(array, array2, 3, 3));
+
+	system("pause>0");
+}
