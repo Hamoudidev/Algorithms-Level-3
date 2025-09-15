536381eddc454692017f9a4e8ed57592e34ab89f
Ahmed Saied <87888343+Hamoudidev@users.noreply.github.com>
2025-09-16 02:06:52 +0300

    Create 24# Upper FirstLatter.cpp

---
+++ 21-30/24# Upper FirstLatter.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+string ReadWord(string Message) {
+	string Word;
+	cout << Message << endl;
+	getline(cin, Word);
+	return Word;
+}
+
+char upperLatter(char Latter) {
+	char x = toupper(Latter);
+	return x;
+}
+
+void upperFirstLatter(string Word) {
+	bool isFirstLetter = true;
+	cout << "\nString after conversion:" << endl;
+
+	for (short i = 0; i < Word.length(); i++) {
+		if (Word[i] != ' ' && isFirstLetter) {
+			Word[i] = upperLatter(Word[i]);
+		}
+
+		isFirstLetter = (Word[i] == ' ' ? true : false);
+	}
+
+	cout << Word << endl;
+}
+
+int main() {
+	srand((unsigned)time(NULL));
+
+	string Word = ReadWord("Please Enter Your String?");
+	upperFirstLatter(Word);
+
+	system("pause>0");
+}
